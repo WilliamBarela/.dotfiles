7ddee1addb458c6f62422182d1fa1d5ea37cc39a
William Barela <william.barela.dev@gmail.com>
2018-04-09 17:27:05 -0500

    add first_run_dotfiles.sh to add sourcing of aliases and custom

---
+++ first_run_dotfiles.sh
@@ -0,0 +1,4 @@
+# For first time clone from Github, run this file.
+# Remember to chmod +x first_run_dotfiles.sh
+echo "source ~/.bash_custom" >> ~/.bashrc
+echo "source ~/.bash_aliases" >> ~/.bashrc
